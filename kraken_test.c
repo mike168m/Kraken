fc37c257b94618dbf2c83ce77ee3c582548fb75e
Michael Osei <mosei27@live.com>
2019-10-19 21:16:40 -0700

    Update docs

--- kraken_test.c
+++ kraken_test.c
@@ -1,21 +1,21 @@
 #include "kraken.h"
-#include <gtest/gtest.h>
+//#include <gtest/gtest.h>
 
 
-TEST( kraken_initialize_runtime_test ) 
-{
-    ASSERT_EQ();
-    ASSERT_EQ();
-}
+//TEST( kraken_initialize_runtime_test ) 
+//{
+//    ASSERT_EQ();
+//    ASSERT_EQ();
+//}
 
 
-int main 
-(
-    void
-)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+//int main 
+//(
+//    void
+//)
+//{
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
 
 
